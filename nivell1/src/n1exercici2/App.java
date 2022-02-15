37745e8f1fbdaf891b8cb375546a91dcafd8279c
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-02-15 08:45:45 +0100

    ex2 clean

--- nivell1/src/n1exercici2/App.java
+++ nivell1/src/n1exercici2/App.java
@@ -1,13 +1,17 @@
 package n1exercici2;
-/*
-Fase 2
-Crea una Functional Interface que contingui un mètode abstracte getPiValue (), que retorn a un valor double;
-en una altra classe, instància la interfície i assigneu-li mitjançant una lambda el valor 3.1415.
-Invoca el mètode getPiValue de la instància d'interfície i imprimeix el resultat.
- */
+
 public class App {
 
     public static void main(String[] args) {
 
+        PiValue pi1 = () -> 3.1415;
+
+        System.out.println(pi1.getPiValue());
     }
+
+    @FunctionalInterface
+    public interface PiValue {
+        double getPiValue();
+    }
+
 }
